c6957f87eb5068ffce2e7434484149b7318634ce
ItsaPlant <krzysiekmichalik6@gmail.com>
2021-09-20 14:18:54 +0200

    done app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, abort, make_response, request
 from flask.wrappers import Request
-from werkzeug.wrappers import request
+
 from models import todos
 
 app = Flask(__name__)
@@ -30,13 +30,44 @@ def get_todo(todo_id):
         abort(404)
     return jsonify({"todo": todo})
 
+@app.route("/api/v1/todos/<int:todo_id>", methods=["PUT"])
+def update_todo(todo_id):
+    todo = todos.get(todo_id)
+    if not todo:
+        abort(404)
+    if not request.json:
+        abort(400)
+    data = request.json
+    if any([
+        'title' in data and not isinstance(data.get('title'), str),
+        'description' in data and not isinstance(data.get('description'), str),
+        'done' in data and not isinstance(data.get('done'), bool)
+    ]):
+        abort(400)
+    todo = {
+        'title': data.get('title', todo['title']),
+        'description': data.get('description', todo['description']),
+        'done': data.get('done', todo['done'])
+    }
+    todos.update(todo_id, todo)
+    return jsonify({'todo': todo})
+
+@app.route("/api/v1/todos/<int:todo_id>", methods = ["DELETE"])
+def delete_todo(todo_id):
+    result = todos.delete(todo_id)
+    if not result:
+        abort(404)
+    return jsonify({'result': result})
+
+#ERRORHANDLERs
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found', 'status_code': 404}), 404)
 
 @app.errorhandler(400)
 def bad_request(error):
-    return make_responce(jsonify({'error': 'Bad request', 'status_code': 400}), 400)
+    return make_response(jsonify({'error': 'Bad request', 'status_code': 400}), 400)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models.py
+++ models.py
@@ -26,9 +26,26 @@ class Todos:
         with open('todos.json', "w") as f:
             json.dump(self.todos, f)
 
+    # def update(self, id, data):
+    #     data.pop('csrf_token')
+    #     self.todos[id] = data
+    #     self.save_all()
+
     def update(self, id, data):
-        data.pop('csrf_token')
-        self.todos[id] = data
-        self.save_all()
+        todo = self.get(id)
+        if todo:
+            index = self.todos.index(id)
+            self.todos[index] = data
+            self.save_all()
+            return True
+        return False
+    
+    def delete(self, id):
+        todo = self.get(id)
+        if todo:
+            self.todos.remove(todo)
+            self.save_all()
+            return True
+        return False
 
 todos = Todos()
